bfe965e6ab02ef6b0fdfe90c1e9526b2669ba65c
Francesco Torsello <44674676+francescotorsello@users.noreply.github.com>
2019-05-24 14:27:05 +0200

    Fixed TINY_Reals almost everywhere
    - gtrK_t and ftrK_t still diverge at r=1. It should be fixed with TINY_Reals

--- include/bispec/eom-BSSN/eomBSSNRicciComp.h
+++ include/bispec/eom-BSSN/eomBSSNRicciComp.h
@@ -1,53 +1,125 @@
 /** @file  eomBSSNRicciComp.h
  *  @author Francesco Torsello
  *  @brief The compactified cBSSN terms involving the Ricci tensor in the evolution equations.
- *  @version 2019-05-24T12:57:16
+ *  @version 2019-05-24T13:15:04
  *  @image html BSSNRicciComp.png
  */
 
 Real gRicci( Int m, Int n )
 {
-    return exp(-4 * gconf(m,n)) * gAlp(m,n) * ((2 * gL_r(m,n)
-    /*2*/  * pow2(r_minus(m,n))) / (3. * r_plus(m,n)) - (2 * gsig(m,n)) 
-    /*1*/  / pow2(gA(m,n)) + (2 * Power(r_minus(m,n),4) * gB_rr(m,n))
-    /*1*/  / (3. * gB(m,n) * pow2(r_plus(m,n)) * pow2(gA(m,n))) 
-    /*1*/  + (2 * Power(r_minus(m,n),4) * pow2(gA_r(m,n))) 
-    /*1*/  / (Power(gA(m,n),4) * pow2(r_plus(m,n))) - (2 
-    /*2*/  * Power(r_minus(m,n),4) * pow2(gB_r(m,n))) / (3. 
-    /*2*/  * pow2(r_plus(m,n)) * pow2(gA(m,n)) * pow2(gB(m,n))) - (2
-    /*2*/  * Power(r_minus(m,n),4) * gA_rr(m,n)) / (3. 
-    /*2*/  * pow2(r_plus(m,n)) * pow3(gA(m,n))) + gB_r(m,n) * ((-2 
-    /*3*/  * Power(r_minus(m,n),4) * gA_r(m,n)) / (gB(m,n) 
-    /*3*/  * pow2(r_plus(m,n)) * pow3(gA(m,n))) - (4 * (-1 
-    /*4*/  + pow2(r(m,n))) * (-(pow2(gB(m,n)) * pow2(r(m,n)) 
-    /*5*/  * (gsig(m,n) * pow2(r_plus(m,n)) + pow2(r_minus(m,n)) * (3 
-    /*7*/  + pow2(r(m,n))))) + pow2(gA(m,n)) * pow2(-1 
-    /*5*/  + Power(r(m,n),4)))) / (3. * pow2(gA(m,n)) 
-    /*3*/  * pow3(r_plus(m,n)) * pow3(gB(m,n)) * r(m,n))) - (4 
-    /*2*/  * Power(r_minus(m,n),4) * gA_r(m,n)) / (3. * pow3(r_plus(m,n)) 
-    /*2*/  * pow3(gA(m,n)) * r(m,n)) + (2 * gL(m,n) * (-(1 
-    /*4*/  / r(m,n)) + r(m,n))) / 3.);
+    return (-2 * gAlp(m,n) * gsig(m,n)) / (TINY_Real + exp(4
+    /*2*/  * gconf(m,n)) * Power(gA(m,n),2)) - (2 
+    /*1*/  * Power(r_minus(m,n),4) * gAlp(m,n) * gA_rr(m,n)) 
+    /*0*/  / (TINY_Real + 3 * Power(r_plus(m,n),2) * exp(4 
+    /*2*/  * gconf(m,n)) * Power(gA(m,n),3)) - (2 
+    /*1*/  * Power(r_minus(m,n),4) * gAlp(m,n) * gA_r(m,n) 
+    /*1*/  * gB_r(m,n)) / (TINY_Real + Power(r_plus(m,n),2) * exp(4 
+    /*2*/  * gconf(m,n)) * Power(gA(m,n),3) * gB(m,n)) + (2 
+    /*1*/  * Power(r_minus(m,n),4) * gAlp(m,n) * gB_rr(m,n)) 
+    /*0*/  / (TINY_Real + 3 * Power(r_plus(m,n),2) * exp(4 
+    /*2*/  * gconf(m,n)) * Power(gA(m,n),2) * gB(m,n)) + (2 
+    /*1*/  * gAlp(m,n) * gL_r(m,n) * pow2(r_minus(m,n))) / (TINY_Real
+    /*1*/  + 3 * r_plus(m,n) * exp(4 * gconf(m,n))) + (2 
+    /*1*/  * Power(r_minus(m,n),4) * gAlp(m,n) * pow2(gA_r(m,n))) 
+    /*0*/  / (TINY_Real + Power(r_plus(m,n),2) * exp(4 * gconf(m,n))
+    /*1*/  * Power(gA(m,n),4)) - (2 * Power(r_minus(m,n),4) 
+    /*1*/  * gAlp(m,n) * pow2(gB_r(m,n))) / (TINY_Real + 3 
+    /*1*/  * Power(r_plus(m,n),2) * exp(4 * gconf(m,n)) 
+    /*1*/  * Power(gA(m,n),2) * Power(gB(m,n),2)) + (4 
+    /*1*/  * gAlp(m,n) * gsig(m,n) * gB_r(m,n) * pow3(r(m,n)))
+    /*0*/  / (TINY_Real + 3 * r_plus(m,n) * exp(4 * gconf(m,n)) 
+    /*1*/  * Power(gA(m,n),2) * gB(m,n)) - (8 * gAlp(m,n) 
+    /*1*/  * gB_r(m,n) * pow3(r(m,n))) / (TINY_Real + 3 
+    /*1*/  * Power(r_plus(m,n),3) * exp(4 * gconf(m,n)) 
+    /*1*/  * Power(gB(m,n),3)) + (8 * gAlp(m,n) * gB_r(m,n) 
+    /*1*/  * pow2(r_minus(m,n)) * pow3(r(m,n))) / (TINY_Real + 3 
+    /*1*/  * Power(r_plus(m,n),3) * exp(4 * gconf(m,n)) 
+    /*1*/  * Power(gA(m,n),2) * gB(m,n)) + (2 * gAlp(m,n) 
+    /*1*/  * gL(m,n) * r(m,n)) / (TINY_Real + 3 * exp(4 
+    /*2*/  * gconf(m,n))) - (4 * gAlp(m,n) * gsig(m,n) 
+    /*1*/  * gB_r(m,n) * r(m,n)) / (TINY_Real + 3 * r_plus(m,n) 
+    /*1*/  * exp(4 * gconf(m,n)) * Power(gA(m,n),2) * gB(m,n))
+    /*0*/  - (4 * gAlp(m,n) * gB_r(m,n) * r(m,n)) 
+    /*0*/  / (TINY_Real + 3 * Power(r_plus(m,n),3) * exp(4 
+    /*2*/  * gconf(m,n)) * Power(gB(m,n),3)) - (4 * gAlp(m,n) 
+    /*1*/  * gB_r(m,n) * pow2(r_minus(m,n)) * r(m,n)) / (TINY_Real 
+    /*1*/  + Power(r_plus(m,n),3) * exp(4 * gconf(m,n)) 
+    /*1*/  * Power(gA(m,n),2) * gB(m,n)) + (8 * gAlp(m,n) 
+    /*1*/  * gB_r(m,n) * Power(r(m,n),5)) / (TINY_Real + 3 
+    /*1*/  * Power(r_plus(m,n),3) * exp(4 * gconf(m,n)) 
+    /*1*/  * Power(gB(m,n),3)) + (4 * gAlp(m,n) * gB_r(m,n) 
+    /*1*/  * pow2(r_minus(m,n)) * Power(r(m,n),5)) / (TINY_Real + 3 
+    /*1*/  * Power(r_plus(m,n),3) * exp(4 * gconf(m,n)) 
+    /*1*/  * Power(gA(m,n),2) * gB(m,n)) + (4 * gAlp(m,n) 
+    /*1*/  * gB_r(m,n) * Power(r(m,n),7)) / (TINY_Real + 3 
+    /*1*/  * Power(r_plus(m,n),3) * exp(4 * gconf(m,n)) 
+    /*1*/  * Power(gB(m,n),3)) - (4 * gAlp(m,n) * gB_r(m,n) 
+    /*1*/  * Power(r(m,n),9)) / (TINY_Real + 3 * Power(r_plus(m,n),3)
+    /*1*/  * exp(4 * gconf(m,n)) * Power(gB(m,n),3)) - (2 
+    /*1*/  * gAlp(m,n) * gL(m,n)) / (TINY_Real + 3 * exp(4 
+    /*2*/  * gconf(m,n)) * r(m,n)) - (4 * Power(r_minus(m,n),4) 
+    /*1*/  * gAlp(m,n) * gA_r(m,n)) / (TINY_Real + 3 
+    /*1*/  * Power(r_plus(m,n),3) * exp(4 * gconf(m,n)) 
+    /*1*/  * Power(gA(m,n),3) * r(m,n)) + (4 * gAlp(m,n) 
+    /*1*/  * gB_r(m,n)) / (TINY_Real + 3 * Power(r_plus(m,n),3) 
+    /*1*/  * exp(4 * gconf(m,n)) * Power(gB(m,n),3) * r(m,n));
 }
 Real fRicci( Int m, Int n )
 {
-    return exp(-4 * fconf(m,n)) * fAlp(m,n) * ((2 * fL_r(m,n)
-    /*2*/  * pow2(r_minus(m,n))) / (3. * r_plus(m,n)) - (2 * fsig(m,n)) 
-    /*1*/  / pow2(fA(m,n)) + (2 * Power(r_minus(m,n),4) * fB_rr(m,n))
-    /*1*/  / (3. * fB(m,n) * pow2(r_plus(m,n)) * pow2(fA(m,n))) 
-    /*1*/  + (2 * Power(r_minus(m,n),4) * pow2(fA_r(m,n))) 
-    /*1*/  / (Power(fA(m,n),4) * pow2(r_plus(m,n))) - (2 
-    /*2*/  * Power(r_minus(m,n),4) * pow2(fB_r(m,n))) / (3. 
-    /*2*/  * pow2(r_plus(m,n)) * pow2(fA(m,n)) * pow2(fB(m,n))) - (2
-    /*2*/  * Power(r_minus(m,n),4) * fA_rr(m,n)) / (3. 
-    /*2*/  * pow2(r_plus(m,n)) * pow3(fA(m,n))) + fB_r(m,n) * ((-2 
-    /*3*/  * Power(r_minus(m,n),4) * fA_r(m,n)) / (fB(m,n) 
-    /*3*/  * pow2(r_plus(m,n)) * pow3(fA(m,n))) - (4 * (-1 
-    /*4*/  + pow2(r(m,n))) * (-(pow2(fB(m,n)) * pow2(r(m,n)) 
-    /*5*/  * (fsig(m,n) * pow2(r_plus(m,n)) + pow2(r_minus(m,n)) * (3 
-    /*7*/  + pow2(r(m,n))))) + pow2(fA(m,n)) * pow2(-1 
-    /*5*/  + Power(r(m,n),4)))) / (3. * pow2(fA(m,n)) 
-    /*3*/  * pow3(r_plus(m,n)) * pow3(fB(m,n)) * r(m,n))) - (4 
-    /*2*/  * Power(r_minus(m,n),4) * fA_r(m,n)) / (3. * pow3(r_plus(m,n)) 
-    /*2*/  * pow3(fA(m,n)) * r(m,n)) + (2 * fL(m,n) * (-(1 
-    /*4*/  / r(m,n)) + r(m,n))) / 3.);
+    return (-2 * fAlp(m,n) * fsig(m,n)) / (TINY_Real + exp(4
+    /*2*/  * fconf(m,n)) * Power(fA(m,n),2)) - (2 
+    /*1*/  * Power(r_minus(m,n),4) * fAlp(m,n) * fA_rr(m,n)) 
+    /*0*/  / (TINY_Real + 3 * Power(r_plus(m,n),2) * exp(4 
+    /*2*/  * fconf(m,n)) * Power(fA(m,n),3)) - (2 
+    /*1*/  * Power(r_minus(m,n),4) * fAlp(m,n) * fA_r(m,n) 
+    /*1*/  * fB_r(m,n)) / (TINY_Real + Power(r_plus(m,n),2) * exp(4 
+    /*2*/  * fconf(m,n)) * Power(fA(m,n),3) * fB(m,n)) + (2 
+    /*1*/  * Power(r_minus(m,n),4) * fAlp(m,n) * fB_rr(m,n)) 
+    /*0*/  / (TINY_Real + 3 * Power(r_plus(m,n),2) * exp(4 
+    /*2*/  * fconf(m,n)) * Power(fA(m,n),2) * fB(m,n)) + (2 
+    /*1*/  * fAlp(m,n) * fL_r(m,n) * pow2(r_minus(m,n))) / (TINY_Real
+    /*1*/  + 3 * r_plus(m,n) * exp(4 * fconf(m,n))) + (2 
+    /*1*/  * Power(r_minus(m,n),4) * fAlp(m,n) * pow2(fA_r(m,n))) 
+    /*0*/  / (TINY_Real + Power(r_plus(m,n),2) * exp(4 * fconf(m,n))
+    /*1*/  * Power(fA(m,n),4)) - (2 * Power(r_minus(m,n),4) 
+    /*1*/  * fAlp(m,n) * pow2(fB_r(m,n))) / (TINY_Real + 3 
+    /*1*/  * Power(r_plus(m,n),2) * exp(4 * fconf(m,n)) 
+    /*1*/  * Power(fA(m,n),2) * Power(fB(m,n),2)) + (4 
+    /*1*/  * fAlp(m,n) * fsig(m,n) * fB_r(m,n) * pow3(r(m,n)))
+    /*0*/  / (TINY_Real + 3 * r_plus(m,n) * exp(4 * fconf(m,n)) 
+    /*1*/  * Power(fA(m,n),2) * fB(m,n)) - (8 * fAlp(m,n) 
+    /*1*/  * fB_r(m,n) * pow3(r(m,n))) / (TINY_Real + 3 
+    /*1*/  * Power(r_plus(m,n),3) * exp(4 * fconf(m,n)) 
+    /*1*/  * Power(fB(m,n),3)) + (8 * fAlp(m,n) * fB_r(m,n) 
+    /*1*/  * pow2(r_minus(m,n)) * pow3(r(m,n))) / (TINY_Real + 3 
+    /*1*/  * Power(r_plus(m,n),3) * exp(4 * fconf(m,n)) 
+    /*1*/  * Power(fA(m,n),2) * fB(m,n)) + (2 * fAlp(m,n) 
+    /*1*/  * fL(m,n) * r(m,n)) / (TINY_Real + 3 * exp(4 
+    /*2*/  * fconf(m,n))) - (4 * fAlp(m,n) * fsig(m,n) 
+    /*1*/  * fB_r(m,n) * r(m,n)) / (TINY_Real + 3 * r_plus(m,n) 
+    /*1*/  * exp(4 * fconf(m,n)) * Power(fA(m,n),2) * fB(m,n))
+    /*0*/  - (4 * fAlp(m,n) * fB_r(m,n) * r(m,n)) 
+    /*0*/  / (TINY_Real + 3 * Power(r_plus(m,n),3) * exp(4 
+    /*2*/  * fconf(m,n)) * Power(fB(m,n),3)) - (4 * fAlp(m,n) 
+    /*1*/  * fB_r(m,n) * pow2(r_minus(m,n)) * r(m,n)) / (TINY_Real 
+    /*1*/  + Power(r_plus(m,n),3) * exp(4 * fconf(m,n)) 
+    /*1*/  * Power(fA(m,n),2) * fB(m,n)) + (8 * fAlp(m,n) 
+    /*1*/  * fB_r(m,n) * Power(r(m,n),5)) / (TINY_Real + 3 
+    /*1*/  * Power(r_plus(m,n),3) * exp(4 * fconf(m,n)) 
+    /*1*/  * Power(fB(m,n),3)) + (4 * fAlp(m,n) * fB_r(m,n) 
+    /*1*/  * pow2(r_minus(m,n)) * Power(r(m,n),5)) / (TINY_Real + 3 
+    /*1*/  * Power(r_plus(m,n),3) * exp(4 * fconf(m,n)) 
+    /*1*/  * Power(fA(m,n),2) * fB(m,n)) + (4 * fAlp(m,n) 
+    /*1*/  * fB_r(m,n) * Power(r(m,n),7)) / (TINY_Real + 3 
+    /*1*/  * Power(r_plus(m,n),3) * exp(4 * fconf(m,n)) 
+    /*1*/  * Power(fB(m,n),3)) - (4 * fAlp(m,n) * fB_r(m,n) 
+    /*1*/  * Power(r(m,n),9)) / (TINY_Real + 3 * Power(r_plus(m,n),3)
+    /*1*/  * exp(4 * fconf(m,n)) * Power(fB(m,n),3)) - (2 
+    /*1*/  * fAlp(m,n) * fL(m,n)) / (TINY_Real + 3 * exp(4 
+    /*2*/  * fconf(m,n)) * r(m,n)) - (4 * Power(r_minus(m,n),4) 
+    /*1*/  * fAlp(m,n) * fA_r(m,n)) / (TINY_Real + 3 
+    /*1*/  * Power(r_plus(m,n),3) * exp(4 * fconf(m,n)) 
+    /*1*/  * Power(fA(m,n),3) * r(m,n)) + (4 * fAlp(m,n) 
+    /*1*/  * fB_r(m,n)) / (TINY_Real + 3 * Power(r_plus(m,n),3) 
+    /*1*/  * exp(4 * fconf(m,n)) * Power(fB(m,n),3) * r(m,n));
 }
